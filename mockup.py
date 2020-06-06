d70378f1bf4af252f421f03dfbf745343e47881e
Jackson Nieto <jacksonnieto@Jacksons-MacBook-Pro.local>
2020-06-06 01:49:21 -0500

    Created simple python mockup of how the game should run

---
+++ mockup.py
@@ -0,0 +1,26 @@
+#!/bin/python
+
+
+print("Player 1 enter rock, paper, or scissors")
+player1_choice = input()
+print("Player 2 enter rock, paper, or scissors")
+player2_choice = input()
+
+
+def evaluation(p1, p2):
+    if p1 == "rock" and p2 == "scissors":
+        return("Player 1 Wins!")
+    elif p2 == "rock" and p1 == "scissors":
+        return("Player 2 Wins!")
+    elif p1 == "rock" and p2 == "paper":
+        return("Player 2 Wins!")
+    elif p2 == "rock" and p1 == "paper":
+        return("Player 1 Wins!")
+    elif p1 == "scissors" and p2 == "paper":
+        return("Player 1 Wins!")
+    elif p2 == "scissors" and p1 == "paper":
+        return("Player 2 Wins!")
+    elif p1 == p2:
+        return("It's a draw!")
+
+print(evaluation(player1_choice, player2_choice))
